078caa861c0468a82cd80d7c7ec5e63eec0bd79a
AyhamAM10 <A1b2c3d4321am10>
2025-06-01 19:14:06 +0300

    fix

--- src/routes/notification.ts
+++ src/routes/notification.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { NotificationController } from "../controllers/Notification.controller";
+import { authMiddleware } from "../middleware/auth.middleware";
+import { checkRole } from "../middleware/checkRole.middleware";
+import { UserRole } from "../entity/User";
 
 
 const notificationRoutes = Router();
 const controller = new NotificationController();
 
-notificationRoutes.get("/user", controller.getUserNotifications.bind(controller));
-notificationRoutes.post("/register-token", controller.registerToken.bind(controller));
-notificationRoutes.post("/send-notification", controller.sendNotification.bind(controller));
+notificationRoutes.get("/user", authMiddleware , controller.getUserNotifications.bind(controller));
+notificationRoutes.post("/register-token", authMiddleware , controller.registerToken.bind(controller));
+notificationRoutes.post("/send-notification", authMiddleware ,checkRole([UserRole.superAdmin]), controller.sendNotification.bind(controller));
 export default notificationRoutes;
