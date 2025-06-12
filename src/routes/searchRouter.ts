505a8abcc3b7ef0b962655d5f872491a79fb8ca6
AyhamAM10 <A1b2c3d4321am10>
2025-06-12 23:29:13 +0300

    fix route

--- src/routes/searchRouter.ts
+++ src/routes/searchRouter.ts
@@ -1,29 +1,19 @@
 import { Router } from "express";
-import { authMiddleware } from "../middleware/auth.middleware";
-import { upload } from "../middleware/uploadProfile";
-import { checkRole } from "../middleware/checkRole.middleware";
-import { UserRole } from "../entity/User";
 import { optionalAuthMiddleware } from "../middleware/optionalAuthMiddleware";
-import { PropertySearchController } from "../controllers/propertySearch.controller copy";
 import { CarSearchController } from "../controllers/carSearch2.controller";
-import { NextFunction , Request , Response } from "express-serve-static-core";
-
-
+import { PropertySearchController } from "../controllers/propertySearch.controller";
 
 const SearchRouter: Router = Router();
 
 const propertyController = new PropertySearchController();
-const carControoler = new CarSearchController()
+const carControoler = new CarSearchController();
 
-SearchRouter.post("/properties",
-    optionalAuthMiddleware,
-    propertyController.search
+SearchRouter.post(
+  "/properties",
+  optionalAuthMiddleware,
+  propertyController.search
 );
 
-SearchRouter.post("/cars",
-    optionalAuthMiddleware,
-    carControoler.search
-);
-
+SearchRouter.post("/cars", optionalAuthMiddleware, carControoler.search);
 
 export default SearchRouter;
