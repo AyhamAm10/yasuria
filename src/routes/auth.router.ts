70f7023477eb620f08cafe7c9119d70162672be8
AyhamAM10 <A1b2c3d4321am10>
2025-07-18 20:32:30 +0300

    fix user bugs

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
+import { authMiddleware } from "../middleware/auth.middleware";
 
 const authRouter:Router = Router();
 
-authRouter.get("/me", AuthController.me);
+authRouter.get("/me", authMiddleware ,  AuthController.me);
 authRouter.post("/login", AuthController.login );
 authRouter.post("/regester", AuthController.register );
 authRouter.post("/logout", AuthController.logout);
